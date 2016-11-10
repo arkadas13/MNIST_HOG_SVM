68d614b7110582bf0d20cb58822a77b64db2e54f
Arka Das <rkdas.das@gmail.com>
2016-11-10 23:22:56 +0530

    Add files via upload

---
+++ HogFeatureExtraction.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import cv2
+from skimage.feature import hog
+from skimage import color, exposure
+
+
+image = color.rgb2gray(cv2.imread("image.jpg"))
+
+fd, hog_image = hog(image, orientations=8, pixels_per_cell=(16, 16),
+                    cells_per_block=(1, 1), visualise=True)
+
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4), sharex=True, sharey=True)
+
+ax1.axis('off')
+ax1.imshow(image, cmap=plt.cm.gray)
+ax1.set_title('Input image')
+
+# Rescale histogram for better display
+hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 0.05))
+
+ax2.axis('off')
+ax2.imshow(hog_image_rescaled, cmap=plt.cm.gray)
+ax2.set_title('Histogram of Oriented Gradients')
+plt.show()
+
+print(fd)
